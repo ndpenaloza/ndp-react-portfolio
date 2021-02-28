2d30be7964cea7e54c7b7ad5e1d2571cae16e0f1
NDPenaloza <ndpenaloza@gmail.com>
2021-02-28 00:26:08 -0800

    added footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,19 @@
+function Footer() {
+  return(
+    <div>
+      <div className="footer text-center">
+        <div className="container text-center">
+          <span className="text-muted text-center">
+            <p>Nick D. Penaloza &copy; 2021</p>
+            <p>ndpenaloza@gmail.com</p>
+            <p>619-734-5193</p>
+          </span>
+        </div>  
+        <div className="container text-center feet-space">
+          <a href="https://github.com/ndpenaloza" className="fa fa-github fa-2x social-link float-center" target="_blank"></a>
+          <a href="https://www.linkedin.com/in/nick-d-penaloza-534a9229" className="fa fa-linkedin fa-2x social-link" target="_blank"></a>
+        </div>
+      </div>
+    </div>
+  )
+}export default Footer;
