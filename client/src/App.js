238ec4d7c05bc1257bcf74d44259aacc41ce87cc
NDPenaloza <ndpenaloza@gmail.com>
2021-03-01 01:13:46 -0800

    began working on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Header from './components/Header';
 
 function App() {
   return (
+    <Router>
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path={}
+        </Switch>
 
+      </div>
+    </Router>
   );
 }
 
