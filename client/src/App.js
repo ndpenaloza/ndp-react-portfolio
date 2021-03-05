ca07890e5f2fdbbd0e76e5bc23d03f986f92a82f
NDPenaloza <ndpenaloza@gmail.com>
2021-03-04 21:55:40 -0800

    may have fixed the link for the resume or some shit

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Index from './pages/Index';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
