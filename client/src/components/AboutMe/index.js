3043e11d9df19532086f2634162c9f730a51c6df
NDPenaloza <ndpenaloza@gmail.com>
2021-03-04 23:48:49 -0800

    commented out next and prev in carousel

--- client/src/components/AboutMe/index.js
+++ client/src/components/AboutMe/index.js
@@ -1,4 +1,3 @@
-// import {Link} from 'react-router-dom';
 import BridalVeil from '../../images/bridalVeil.jpg';
 import DelicateArch from '../../images/delicateArch.jpg';
 import GoldenGate from '../../images/goldenGate.jpg';
@@ -39,14 +38,14 @@ function AboutMe() {
                       <img className="d-block w-100" src={DelicateArch} alt="The Delicate Arch"/>
                     </div>
                   </div>
-                    <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span className="sr-only">Previous</span>
-                    </a>
-                    <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                      <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span className="sr-only">Next</span>
-                    </a>
+                  {/* <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                  </a>
+                  <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                  </a> */}
                 </div>
               </div>
             </div>
