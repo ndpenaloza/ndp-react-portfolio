160e4e6f37b6e5a1a5d001a22c3f9b8c2e6c7e50
NDPenaloza <ndpenaloza@gmail.com>
2021-03-04 19:28:48 -0800

    changed a to Link

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,23 +1,24 @@
+import { Link } from 'react-router-dom';
 import NDPResume from '../../images/NDPenaloza-Resume.pdf'
 
 function Header() {
   return(
     <div>
       <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-light">
-            <a className="navbar-brand" href="/"><h2 style={{color: '#1A5276'}}>Nick D. Penaloza</h2></a>
+            <Link className="navbar-brand" to="/"><h2 style={{color: '#1A5276'}}>Nick D. Penaloza</h2></Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <a className="nav-link" href="/portfolio">Portfolio</a>
+                  <Link className="nav-link" to="/portfolio">Portfolio</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/contact">Contact</a>
+                  <Link className="nav-link" to="/contact">Contact</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href={NDPResume} target="_blank" rel="noreferrer">Resume</a>
+                  <Link className="nav-link" to={NDPResume} target="_blank" rel="noreferrer">Resume</Link>
                 </li>
               </ul>
             </div>
