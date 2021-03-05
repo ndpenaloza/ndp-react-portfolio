64088cbde1a05352fef299cf387abf89d8358ad4
NDPenaloza <ndpenaloza@gmail.com>
2021-03-04 21:28:41 -0800

    changed around app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,16 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Index from './pages/Index';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
-import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <div>
-        <Header/>
           <Switch>
-            <Route exact path='/'>
-              <Index/>
-            </Route>
-            <Route exact path='/portfolio'>
-              <Portfolio/>
-            </Route>
-            <Route exact path='/contact'>
-              <Contact/>
-            </Route>
+            <Route exact path='/' component={Index}/>
+            <Route exact path='/portfolio' component ={Portfolio}/>
+            <Route exact path='/contact' component={Contact}/>
           </Switch>
         <Footer/>
       </div>
