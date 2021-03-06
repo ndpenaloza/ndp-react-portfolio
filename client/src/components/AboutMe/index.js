ad4ec6c76daa6a416fbc183f40fcb86ca0e40373
NDPenaloza <ndpenaloza@gmail.com>
2021-03-05 17:32:45 -0800

    imported bootstrap js stuff and added dependeny

--- client/src/components/AboutMe/index.js
+++ client/src/components/AboutMe/index.js
@@ -38,14 +38,14 @@ function AboutMe() {
                       <img className="d-block w-100" src={DelicateArch} alt="The Delicate Arch"/>
                     </div>
                   </div>
-                  {/* <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                  <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="sr-only">Previous</span>
                   </a>
                   <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="sr-only">Next</span>
-                  </a> */}
+                  </a>
                 </div>
               </div>
             </div>
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
   <React.StrictMode>
